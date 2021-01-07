4126e4df8036b305efb0828b19cf51293c97606f
Airat Zalialiev <zalyaliev.evermore@gmail.com>
2021-01-07 20:10:45 +0300

    initial

---
+++ headers/vector.hpp
@@ -0,0 +1,12 @@
+#ifndef VECTOR_HPP
+# define VECTOR_HPP
+
+namespace ft {
+    
+    template < typename T, 
+    class Vector {
+
+    };
+}
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "headers/vector.hpp"
+
+// 1. Placement new
+// new (ptr) T();
+
+// 2. Явный вызов деструктора
+// p->~T();
+
+// 3. Перегрузка оператора new
+// static void* operator new(size_t n) {
+//     ::operator new( );
+// }
+
+
+
+
+int main(void) {
+
+    return (0);
+}
