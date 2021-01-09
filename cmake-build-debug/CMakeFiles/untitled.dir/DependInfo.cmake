e4433ea9ff8aff33a8a167b4b39c1737a215ce3d
AiratZa <zalyaliev.evermore@gmail.com>
2021-01-09 12:54:12 +0300

    readme added

--- cmake-build-debug/CMakeFiles/untitled.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/untitled.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/gdrake/CLionProjects/untitled/main.cpp" "/Users/gdrake/CLionProjects/untitled/cmake-build-debug/CMakeFiles/untitled.dir/main.cpp.o"
+  "/Users/gdrake/CLionProjects/containers/main.cpp" "/Users/gdrake/CLionProjects/containers/cmake-build-debug/CMakeFiles/untitled.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/gdrake/CLionProjects/untitled
+# Install script for directory: /Users/gdrake/CLionProjects/containers
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/gdrake/CLionProjects/untitled/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/gdrake/CLionProjects/containers/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
