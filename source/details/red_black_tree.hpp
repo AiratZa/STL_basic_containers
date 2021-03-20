4547a0663c67f624c2d3666a8cfe3877a7bf58b0
Airat Zalialiev <zalyaliev.evermore@gmail.com>
2021-03-20 10:43:27 +0300

    some changes

--- source/details/red_black_tree.hpp
+++ source/details/red_black_tree.hpp
@@ -5,4 +5,155 @@
 #ifndef FT_CONTAINERS_RED_BLACK_TREE_HPP
 #define FT_CONTAINERS_RED_BLACK_TREE_HPP
 
+#include "allocator_traits_wrapper.hpp"
+
+namespace ft {
+
+namespace __details {
+
+    enum _RB_tree_color {
+        _RED = false,
+        _BLACK = true
+    };
+
+    struct _RB_tree_node_base
+    {
+        typedef _RB_tree_node_base* _Base_ptr;
+        typedef const _RB_tree_node_base* _Const_Base_ptr;
+
+        _RB_tree_color	_color;
+        _Base_ptr		_parent;
+        _Base_ptr		_left;
+        _Base_ptr		_right;
+
+//        static _Base_ptr
+//        _S_minimum(_Base_ptr __x) _GLIBCXX_NOEXCEPT
+//        {
+//            while (__x->_M_left != 0) __x = __x->_M_left;
+//            return __x;
+//        }
+//
+//        static _Const_Base_ptr
+//        _S_minimum(_Const_Base_ptr __x) _GLIBCXX_NOEXCEPT
+//        {
+//            while (__x->_M_left != 0) __x = __x->_M_left;
+//            return __x;
+//        }
+
+//        static _Base_ptr
+//        _S_maximum(_Base_ptr __x) _GLIBCXX_NOEXCEPT
+//        {
+//            while (__x->_M_right != 0) __x = __x->_M_right;
+//            return __x;
+//        }
+//
+//        static _Const_Base_ptr
+//        _S_maximum(_Const_Base_ptr __x) _GLIBCXX_NOEXCEPT
+//        {
+//            while (__x->_M_right != 0) __x = __x->_M_right;
+//            return __x;
+//        }
+    };
+
+    // Helper type offering value initialization guarantee on the compare functor.
+    template<typename key_compare>
+    struct _RB_tree_key_compare
+    {
+        key_compare		_key_compare;
+
+        _RB_tree_key_compare() : _key_compare() { }
+
+        _RB_tree_key_compare(const key_compare& _comp)
+                : _key_compare(_comp) { }
+
+    };
+
+    // Helper type to manage default initialization of node count and header.
+    struct _RB_tree_header
+    {
+        _RB_tree_node_base	_header;
+        std::size_t		_node_count; // Keeps track of size of tree.
+
+        _Rb_tree_header()
+        {
+            _header._color = _RED;
+            _reset();
+        }
+
+//        void
+//        _M_move_data(_Rb_tree_header& __from)
+//        {
+//            _M_header._M_color = __from._M_header._M_color;
+//            _M_header._M_parent = __from._M_header._M_parent;
+//            _M_header._M_left = __from._M_header._M_left;
+//            _M_header._M_right = __from._M_header._M_right;
+//            _M_header._M_parent->_M_parent = &_M_header;
+//            _M_node_count = __from._M_node_count;
+//
+//            __from._M_reset();
+//        }
+
+        void _reset()
+        {
+            _header._parent = 0;
+            _header._left = &_header;
+            _header._right = &_header;
+            _node_count = 0;
+        }
+    };
+
+    template<typename value_type>
+    struct _RB_tree_node : public _RB_tree_node_base
+    {
+        typedef _Rb_tree_node<value_type>* _Link_type;
+
+        value_type _data;
+
+        value_type* _getDataPtr()
+        { return &_data; }
+
+        const value_type*  _getDataPtr() const
+        { return &_data; }
+
+    };
+
+
+    template<typename _Key, typename _Val, typename _KeyOfValue,
+            typename _Compare, typename _Alloc = allocator<_Val> >
+    class _RB_tree
+    {
+        typedef typename alloc_traits_wrapper<_Alloc>::template
+        rebind<_RB_tree_node<_Val> >::other _Node_allocator;
+
+        typedef alloc_traits_wrapper<_Node_allocator> _Alloc_traits;
+
+    protected:
+        typedef _RB_tree_node_base* 		_Base_ptr;
+        typedef const _RB_tree_node_base* 	_Const_Base_ptr;
+        typedef _RB_tree_node<_Val>* 		_Link_type;
+        typedef const _RB_tree_node<_Val>*	_Const_Link_type;
+
+    public:
+        typedef _Key 				key_type;
+        typedef _Val 				value_type;
+        typedef value_type* 			pointer;
+        typedef const value_type* 		const_pointer;
+        typedef value_type& 			reference;
+        typedef const value_type& 		const_reference;
+        typedef std::size_t 				size_type;
+        typedef std::ptrdiff_t 			difference_type;
+        typedef _Alloc 				allocator_type;
+
+
+
+
+
+
+    };
+
+
+
+}
+}
+
 #endif //FT_CONTAINERS_RED_BLACK_TREE_HPP
--- source/map.hpp
+++ source/map.hpp
@@ -12,80 +12,80 @@ namespace ft {
 
     // MAP ITSELF STARTS
 
-//    template < class Key,                                     // map::key_type
-//            class T,                                       // map::mapped_type
-//            class Compare = less<Key>,                     // map::key_compare
-//            class Alloc = allocator<pair<const Key,T> > >    // map::allocator_type
-//    class map {
-//
-//    public:
-////// Member types
-//        typedef Key key_type;
-//        typedef T mapped_type;
-//        typedef std::pair<const key_type,mapped_type> value_type;
-//
-//        typedef Compare key_compare;
-//
-//
-//        class value_compare
-//                : public std::binary_function<value_type, value_type, bool> {
-//            friend class map<Key, T, Compare, Alloc>;
-//
-//            protected:
-//                Compare comp;
-//
-//                value_compare(Compare c)
-//                        : comp(c) { }
-//
-//            public:
-//                bool operator()(const value_type& _x, const value_type& _y) const
-//                { return comp(_x.first, _y.first); }
-//        };
-//
-//        typedef Alloc allocator_type;
-//
-//    private:
-//        /// This turns a red-black tree into a [multi]map.
-//        typedef typename __custom::alloc_traits_wrapper<_Alloc>::template
-//        rebind<value_type>::other _Pair_alloc_type;
-//
-//        typedef _RB_tree<key_type, value_type, _Select1st<value_type>,
-//        key_compare, _Pair_alloc_type> _RB_tree_type;
-//
-//        /// The actual tree structure.
-//        _RB_tree_type _tree;
-//
-//        typedef __custom::alloc_traits_wrapper<_Pair_alloc_type> _Alloc_traits;
-//
-//    public:
-//        typedef typename _Alloc_traits::pointer		 pointer;
-//        typedef typename _Alloc_traits::const_pointer	 const_pointer;
-//        typedef typename _Alloc_traits::reference		 reference;
-//        typedef typename _Alloc_traits::const_reference	 const_reference;
-//        typedef typename _RB_tree_type::iterator		 iterator;
-//        typedef typename _RB_tree_type::const_iterator	 const_iterator;
-//        typedef typename _RB_tree_type::size_type		 size_type;
-//        typedef typename _RB_tree_type::difference_type	 difference_type;
-//        typedef typename _RB_tree_type::reverse_iterator	 reverse_iterator;
-//        typedef typename _RB_tree_type::const_reverse_iterator const_reverse_iterator;
-//
-//
-//
-////// Member functions
-//
-//// Constructor(s)
-//
-//
-//        // empty (1)
-//        explicit map (const key_compare& comp = key_compare(),
-//                      const allocator_type& alloc = allocator_type())
-//              : : _tree(comp, _Pair_alloc_type(alloc)) { };
-//
-//// Other member functions
-//
-//
-//
-//    };
+    template < class Key,                                     // map::key_type
+            class T,                                       // map::mapped_type
+            class Compare = less<Key>,                     // map::key_compare
+            class Alloc = allocator<pair<const Key,T> > >    // map::allocator_type
+    class map {
+
+    public:
+//// Member types
+        typedef Key key_type;
+        typedef T mapped_type;
+        typedef std::pair<const key_type,mapped_type> value_type;
+
+        typedef Compare key_compare;
+
+
+        class value_compare
+                : public std::binary_function<value_type, value_type, bool> {
+            friend class map<Key, T, Compare, Alloc>;
+
+            protected:
+                Compare comp;
+
+                value_compare(Compare c)
+                        : comp(c) { }
+
+            public:
+                bool operator()(const value_type& _x, const value_type& _y) const
+                { return comp(_x.first, _y.first); }
+        };
+
+        typedef Alloc allocator_type;
+
+    private:
+        /// This turns a red-black tree into a [multi]map.
+        typedef typename __details::alloc_traits_wrapper<_Alloc>::template
+        rebind<value_type>::other _Pair_alloc_type;
+
+        typedef _RB_tree<key_type, value_type, _Select1st<value_type>,
+        key_compare, _Pair_alloc_type> _RB_tree_type;
+
+        /// The actual tree structure.
+        _RB_tree_type _tree;
+
+        typedef __custom::alloc_traits_wrapper<_Pair_alloc_type> _Alloc_traits;
+
+    public:
+        typedef typename _Alloc_traits::pointer		 pointer;
+        typedef typename _Alloc_traits::const_pointer	 const_pointer;
+        typedef typename _Alloc_traits::reference		 reference;
+        typedef typename _Alloc_traits::const_reference	 const_reference;
+        typedef typename _RB_tree_type::iterator		 iterator;
+        typedef typename _RB_tree_type::const_iterator	 const_iterator;
+        typedef typename _RB_tree_type::size_type		 size_type;
+        typedef typename _RB_tree_type::difference_type	 difference_type;
+        typedef typename _RB_tree_type::reverse_iterator	 reverse_iterator;
+        typedef typename _RB_tree_type::const_reverse_iterator const_reverse_iterator;
+
+
+
+//// Member functions
+
+// Constructor(s)
+
+
+        // empty (1)
+        explicit map (const key_compare& comp = key_compare(),
+                      const allocator_type& alloc = allocator_type())
+              : : _tree(comp, _Pair_alloc_type(alloc)) { };
+
+// Other member functions
+
+
+
+    };
 
 // relational operators (stack)
 
